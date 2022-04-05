af51789b9b4c30c3e86c28b16d1462d971b185dc
João Bispo <joaobispo2077@gmail.com>
2022-04-05 11:15:58 -0300

    feat(purchases): setup database

---
+++ backend/purchases/src/database/prisma/prisma.service.ts
@@ -0,0 +1,37 @@
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+    } catch (error) {
+      Logger.log('Error when trying to connect with database');
+      Logger.error(error);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
